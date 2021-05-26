4173f3e684648d3d93af77a8125523c40655cd03
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-05-26 12:05:46 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useEffect,useState} from "react";
 import './App.css';
+import API from "./utils/API";
 
 function App() {
+  const [formState,setFormState] = useState({
+    email:"",
+    password:""
+  })
+
+  const [userState,setUserState] = useState({
+    token:"",
+    user:{
+
+    }
+  })
+
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    API.getProfile(token).then(res=>{
+      console.log(res.data);
+    })
+  },[])
+
+  const handleFormSubmit = e =>{
+    e.preventDefault();
+    API.login(formState).then(res=>{
+      console.log(res.data);
+      localStorage.setItem("token",res.data.token)
+      setUserState({
+        ...userState,
+        token:res.data.token,
+        user:{
+          email:res.data.user.email,
+          name:res.data.user.name,
+        }
+      })
+    }).catch(err=>{
+      console.log("error occured")
+      console.log(err);
+    })
+    setFormState({
+      email:"",
+      password:""
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>welcome!</h1>
+      <form onSubmit = {handleFormSubmit}> 
+        <input name="email" value = {formState.email} onChange={(e)=>setFormState({...formState,email:e.target.value})}/>
+        <input name="password"  type="password" value = {formState.password} onChange={(e)=>setFormState({...formState,password:e.target.value})}/>
+        <input type="submit" value="login"/>
+      </form>
     </div>
   );
 }
